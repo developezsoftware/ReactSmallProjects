b720962881e44f9910da59798e0b28596e6bbd08
Developez Software <88462760+developezsoftware@users.noreply.github.com>
2024-08-28 12:42:42 +0200

    refactor to use context

--- books/src/App.js
+++ books/src/App.js
@@ -1,62 +1,19 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useEffect, useContext } from "react";
 import BookCreate from "./components/BookCreate";
+import BooksContext from "./context/books";
 import BookList from "./components/BookList";
-import axios from 'axios';
 
 function App() {
-    const [books, setBooks] = useState([]);
-
-    const fetchBooks = async () => {
-        const response = await axios.get('http://localhost:3001/books');
-        setBooks(response.data);
-    };
+    const { fetchBooks } = useContext(BooksContext);
 
     useEffect(() => {
         fetchBooks();
     }, []);
 
-    const editBookById = async (id, newTitle) => {
-        const response = await axios.put(`http://localhost:3001/books/${id}`,
-            {title: newTitle});
-
-        const updatedBooks = books.map((book) => {
-            if (book.id === id) {
-            return { ...book, ...response.data };
-            }
-
-            return book;
-        });
-
-        setBooks(updatedBooks);
-    };
-    const deleteBookByID = async (id) => {
-        await axios.delete(`http://localhost:3001/books/${id}`);
-        
-        const updatedBooks = books.filter((book) => {
-            return book.id !== id;
-        });
-        
-        setBooks(updatedBooks);
-    };
-
-    const createBook = async (title) => {
-        const response = await axios.post(
-            'http://localhost:3001/books',
-            {title,}
-        );
-        const updatedBooks = [
-            ...books,
-            response.data
-        ];
-
-        setBooks(updatedBooks);
-    };
-
     return (
         <div className="app">
-            <BookList onEdit={editBookById} books={books} onDelete={deleteBookByID}/>
-            <BookCreate onCreate={createBook}/>
+            <BookList />
+            <BookCreate />
         </div>
     );
 }
